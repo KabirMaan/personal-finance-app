0911cf66e2b0aeaf8700b36b71f0d2c9cf39e8fa
Kabir Maan <kabirmaan11@gmail.com>
2018-04-29 18:32:45 +0100

    Add PrivateRoutes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { shallow } from "enzyme";
 import toJSON from "enzyme-to-json";
-import Header from "../../components/Header";
+import { Header } from "../../components/Header";
+import { start } from "repl";
 
 test("should render Header correctly", () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => {}} />);
   expect(toJSON(wrapper)).toMatchSnapshot();
-  //   expect(wrapper.find("h1").text()).toBe("Personal Finance App");
-  //   const renderer = new ReactShallowRenderer();
-  //   renderer.render(<Header />);
-  //   expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
+test("should call startLogout on button click", () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find("button").simulate("click");
+  expect(startLogout).toHaveBeenCalled();
 });
